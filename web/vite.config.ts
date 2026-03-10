bc4d7a73c169c81cbf0dfcbbf0c6aa43898c83ce
willdonner <willdonner4@gmail.com>
2026-03-10 10:36:24 +0800

    Revert "3005"
    This reverts commit c9d6e9e17bfe1480fe58a28b4f33c9139bad3f29.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -60,16 +60,16 @@ export default defineConfig({
   server: {
     proxy: {
       '/socket.io': {
-        target: 'http://localhost:3005',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         ws: true,
       },
       '/api': {
-        target: 'http://localhost:3005',
+        target: 'http://localhost:3000',
         changeOrigin: true,
       },
       '/game-config': {
-        target: 'http://localhost:3005',
+        target: 'http://localhost:3000',
         changeOrigin: true,
       },
     },
